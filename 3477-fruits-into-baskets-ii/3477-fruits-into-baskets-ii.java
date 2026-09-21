22ee7aa2d5d86756fc770f1f2aefdea2c818fc07
Khush Bisht <khush.bisht267@gmail.com>
2026-09-21 21:53:57 +0530

    Time: 2 ms (72.37%), Space: 46.7 MB (13.3%) - LeetHub

---
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n=fruits.length;
+        int c=n;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(fruits[i]<=baskets[j]){
+                    c--;
+                    baskets[j]=0;
+                    break;
+                }
+                
+            }
+        }
+        return c;
+    }
+}
